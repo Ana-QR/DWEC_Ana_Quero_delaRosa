f43f6d31b253c39f26b9a5691a9d86f1b406d706
anaqu <anaquero1411@gmail.com>
2025-03-08 02:55:26 +0100

    tarea6 sin readme

---
+++ Task6-Asynchronous_communication/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./dist/**/*.html",
+    "./dist/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
